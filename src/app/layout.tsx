e7811531987270e0627d0f9e045bc1a1e0a56123
Gean Ferreira <gean_roberto@hotmail.com>
2025-04-25 08:40:34 -0300

    feat: Integrate Vercel Analytics for enhanced tracking in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import '@/css/globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,6 +39,7 @@ export default function RootLayout({
         <Header />
         <main className='pt-16'>{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
